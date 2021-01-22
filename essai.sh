da48cd677fea2c2ccbb486dda3d2ba4be4c993bc
Manon BAILLET <manon.baillet@etu.univ-nantes.fr>
2021-01-22 12:28:40 +0100

    script + package

---
+++ essai.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+mv package.json ./.node-red
+cd .node-red
+npm install
+node-red demo_v1.json
