08c80c1e59c16320bbef521ae31a1b06db6c1cb2
Florine <florine.germond@etu.univ-nantes.fr>
2021-01-22 15:03:51 +0100

    Update script

--- essai.sh
+++ essai.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade -y
 yes Y|bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 mv package.json ./.node-red
 cd .node-red
 npm install
 cd ..
+sudo systemctl enable nodered.service
 node-red demo_v1.json
