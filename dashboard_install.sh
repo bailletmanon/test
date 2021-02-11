dedfff108918e4072ca2328696ed854d6f2135bc
bailletmanon <61618757+bailletmanon@users.noreply.github.com>
2021-02-11 15:59:38 +0100

    ajout description

---
+++ dashboard_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo"Ce script va installer la boxenergie sur votre raspberry."
+
+echo"Début d'installation de Node-Red et Node.js."
+yes Y|bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+echo"Fin d'installation de Node-Red et Node.js"
+
+echo"Ajout du flow Node-Red de la boxenergie"
+mv package.json ./.node-red
+mv flows_raspberrypi.json ./.node-red
+
+echo"Installation des noeuds requis."
+cd .node-red
+npm install
+cd ..
+
+echo"Activation du lancement automatique de Node-Red au démarrage de votre raspberry" 
+sudo systemctl enable nodered.service
--- essai.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yes Y|bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
-mv package.json ./.node-red
-mv flows_raspberrypi.json ./.node-red
-cd .node-red
-npm install
-cd ..
-sudo systemctl enable nodered.service
