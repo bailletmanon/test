fb0b5dd82e6254433d3819938eea372bacd04e8c
bailletmanon <61618757+bailletmanon@users.noreply.github.com>
2021-02-11 15:48:40 +0100

    Update essai.sh

--- essai.sh
+++ essai.sh
@@ -2,8 +2,8 @@
 
 yes Y|bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 mv package.json ./.node-red
+mv flows_raspberrypi.json ./.node-red
 cd .node-red
 npm install
 cd ..
 sudo systemctl enable nodered.service
-node-red demo_v1.json
