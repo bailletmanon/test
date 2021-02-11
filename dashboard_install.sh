7691924f02e13feb298604855047719957fe2b1e
bailletmanon <61618757+bailletmanon@users.noreply.github.com>
2021-02-11 17:01:15 +0100

    Update dashboard_install.sh

--- dashboard_install.sh
+++ dashboard_install.sh
@@ -5,14 +5,16 @@ echo "Début d'installation de Node-Red et Node.js."
 yes Y|bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 echo "Fin d'installation de Node-Red et Node.js"
 
-echo "Ajout du flow Node-Red de la boxenergie"
+echo "Ajout du flow Node-Red de la boxenergie. "
 mv package.json ./.node-red
 mv flows_raspberrypi.json ./.node-red
 
-echo "Installation des noeuds requis."
+echo "Installation des noeuds supplémentaires requis."
 cd .node-red
 npm install >> /dev/null
 cd ..
 
 echo "Activation du lancement automatique de Node-Red au démarrage de votre raspberry." 
 sudo systemctl enable nodered.service
+node-red-start
+echo "Votre boxenergie est disponible à l'adresse suivante : 127.0.0.1:1880/ui"
